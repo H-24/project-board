8742c6b02798fbeb32856a2c0c4c761f5be69514
H-24 <pyj0318jgmw@gmail.com>
2025-04-15 16:51:00 +0900

    #14 - 도메인 코드를 엔티티로 변환
    생성자와 'equals()', 'hashcode()' 등이 도메인 맥락에 맞게끔 설정함

--- src/main/java/org/example/projectboard/domain/ArticleComments.java
+++ src/main/java/org/example/projectboard/domain/ArticleComments.java
@@ -1,14 +1,61 @@
 package org.example.projectboard.domain;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.stereotype.Indexed;
+
 import java.time.LocalDateTime;
+import java.util.Objects;
 
+@Getter
+@Setter
+@ToString
+@Table(indexes = {
+        @Index(columnList = "content"),
+        @Index(columnList = "createdAt"),
+        @Index(columnList = "createdBy")
+})
+@Entity
 public class ArticleComments {
-    private Long id;
-    private Boards boards;  // 게시글 (ID)
-    private String content; // 내용
 
-    private LocalDateTime createdAt;    // 생성일시
-    private String createdBy;           // 생성자
-    private LocalDateTime modifiedAt;   // 수정일시
-    private String modifiedBy;          // 수정자
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(optional = false) private Boards boards;  // 게시글 (ID)
+    @Column(nullable = false, length = 500) private String content; // 내용
+
+    @CreatedDate @Column(nullable = false) private LocalDateTime createdAt; // 생성일시
+    @CreatedBy @Column(nullable = false, length = 100) private String createdBy;    // 생성자
+    @LastModifiedDate @Column(nullable = false) private LocalDateTime modifiedAt;   // 수정일시
+    @LastModifiedBy @Column(nullable = false, length = 100) private String modifiedBy;  // 수정자
+
+    protected ArticleComments() {}
+
+    private ArticleComments(Boards boards, String content) {
+        this.boards = boards;
+        this.content = content;
+    }
+
+    public static ArticleComments of(Boards boards, String content) {
+        return new ArticleComments(boards, content);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof ArticleComments that)) return false;
+        return id != null && Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
 }
--- src/main/java/org/example/projectboard/domain/Boards.java
+++ src/main/java/org/example/projectboard/domain/Boards.java
@@ -1,15 +1,71 @@
 package org.example.projectboard.domain;
 
-import java.time.LocalDateTime;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
 
+import java.time.LocalDateTime;
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.Objects;
+import java.util.Set;
+
+@Getter
+@Setter
+@ToString      // 객체의 문자열 표현을 자동으로 생성
+@Table(indexes = {
+        @Index(columnList = "title"),
+        @Index(columnList = "hashtag"),
+        @Index(columnList = "createAt"),
+        @Index(columnList = "createdBy")
+})    // 인덱스 기능
+@Entity
 public class Boards {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private String title;   // 제목
-    private String content; // 내용
+
+    @Column(nullable = false, length = 255) private String title;   // 제목
+    @Column(nullable = false, length = 10000) private String content; // 내용
+
     private String hashtag; // 해시태그
 
-    private LocalDateTime createdAt;    // 생성일시
-    private String createdBy;           // 생성자
-    private LocalDateTime modifiedAt;   // 수정일시
-    private String modifiedBy;          // 수정자
+    @OrderBy("id")
+    @OneToMany(mappedBy = "boards", cascade = CascadeType.ALL)
+    private final Set<ArticleComments> articleComments = new LinkedHashSet<>();
+
+    @CreatedDate @Column(nullable = false) private LocalDateTime createdAt;    // 생성일시
+    @CreatedBy @Column(nullable = false, length = 100) private String createdBy;    // 생성자
+    @LastModifiedDate @Column(nullable = false) private LocalDateTime modifiedAt;   // 수정일시
+    @LastModifiedBy @Column(nullable = false, length = 100) private String modifiedBy;  // 수정자
+
+    protected Boards() {}
+
+    private Boards(String title, String content, String hashtag) {
+        this.title = title;
+        this.content = content;
+        this.hashtag = hashtag;
+    }
+
+    public static Boards of(String title, String content, String hashtag) {
+        return new Boards(title, content, hashtag);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Boards boards)) return false;
+        return id !=null && Objects.equals(id, boards.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
 }
