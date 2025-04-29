92bb63660820279f9f3a72b9d0d927b55289e404
H-24 <pyj0318jgmw@gmail.com>
2025-04-29 09:30:16 +0900

    #1 - Querydsl 지원을 이용하여 api 검색 옵션 추가
    'QuerydslPredicateExecutor', 'QuerydslBinderCustomizer'를 이용하여 간단하게 개발

--- src/main/java/org/example/projectboard/repository/BoardsRepository.java
+++ src/main/java/org/example/projectboard/repository/BoardsRepository.java
@@ -1,9 +1,30 @@
 package org.example.projectboard.repository;
 
+import com.querydsl.core.types.dsl.DateTimeExpression;
+import com.querydsl.core.types.dsl.StringExpression;
 import org.example.projectboard.domain.Boards;
+import org.example.projectboard.domain.QBoards;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
+import org.springframework.data.querydsl.binding.QuerydslBinderCustomizer;
+import org.springframework.data.querydsl.binding.QuerydslBindings;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @RepositoryRestResource(path = "boards")
-public interface BoardsRepository extends JpaRepository<Boards, Long> {
+public interface BoardsRepository extends
+        JpaRepository<Boards, Long>,
+        QuerydslPredicateExecutor<Boards>,
+        QuerydslBinderCustomizer<QBoards> {
+
+    @Override
+    default void customize(QuerydslBindings bindings, QBoards root) {
+        bindings.excludeUnlistedProperties(true);
+        bindings.including(root.title, root.content, root.hashtag, root.createdAt, root.createdBy);
+        // bindings.bind(root.title).first(StringExpression::likeIgnoreCase);
+        bindings.bind(root.title).first(StringExpression::containsIgnoreCase);
+        bindings.bind(root.content).first(StringExpression::containsIgnoreCase);
+        bindings.bind(root.hashtag).first(StringExpression::containsIgnoreCase);
+        bindings.bind(root.createdAt).first(DateTimeExpression::eq);
+        bindings.bind(root.createdBy).first(StringExpression::containsIgnoreCase);
+    }
 }
