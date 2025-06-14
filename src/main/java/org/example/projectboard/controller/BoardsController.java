b3f13b0186914db88f056ea4faa0679ba9093e82
H-24 <pyj0318jgmw@gmail.com>
2025-06-14 16:21:15 +0900

    #22 - 게시판 컨트롤러 구현
    게시판 기본 조회 화면을 위한 컨트롤러 구현하고 데이터 전달 완료
    게시판 페이지 기본 정렬 속성 부여: 10개 1페이지, 시간 내림차순

--- src/main/java/org/example/projectboard/controller/BoardsController.java
+++ src/main/java/org/example/projectboard/controller/BoardsController.java
@@ -1,27 +1,44 @@
 package org.example.projectboard.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.example.projectboard.domain.type.SearchType;
+import org.example.projectboard.response.ArticleWithCommentsResponse;
+import org.example.projectboard.response.BoardsResponse;
+import org.example.projectboard.service.BoardService;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
-import java.util.List;
-
+@RequiredArgsConstructor
 @RequestMapping("/boards")
 @Controller
 public class BoardsController {
 
+    private final BoardService boardService;
+
     @GetMapping
-    public String boards(ModelMap model) {
-        model.addAttribute("boards", List.of());
+    public String boards(
+        @RequestParam(name ="searchType", required = false) SearchType searchType,
+        @RequestParam(name = "searchValue", required = false) String searchValue,
+        @PageableDefault(sort = "createdAt", direction = Sort.Direction.DESC) Pageable pageable,
+        ModelMap model
+    ) {
+        model.addAttribute("boards", boardService.searchBoards(searchType, searchValue, pageable).map(BoardsResponse::from));
+
         return "boards/index";
     }
 
     @GetMapping("/{articleId}")
     public String boards(@PathVariable("articleId") Long articleId, ModelMap model) {
-        model.addAttribute("boards", "boards");
-        model.addAttribute("articleComments", List.of());
+        ArticleWithCommentsResponse board = ArticleWithCommentsResponse.from(boardService.getBoard(articleId));
+        model.addAttribute("boards", board);
+        model.addAttribute("articleComments", board.articleCommentsResponse());
 
         return "boards/details";
     }
