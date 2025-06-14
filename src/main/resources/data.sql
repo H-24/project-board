7e86b33824866d537be500d0b53d73d6dc204de6
H-24 <pyj0318jgmw@gmail.com>
2025-06-14 16:31:33 +0900

    #22 - 게시판 뷰 템플릿 구현
    그 밖에 헤더, 푸터 목업 표현을 추가 (서버 렌더링을 안 하고 html을 그냥 열어도 맥락을 파악할 수 있도록)
    +데이터 추가

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -8,7 +8,34 @@ INSERT INTO boards (user_account_id, title, content, hashtag, created_by, create
 VALUES (1, '첫 번째 게시글', '테스트용 내용입니다. 지금 듣고 있는 노래는 개화입니다.', '#개화', 'tester', NOW(), 'tester', NOW());
 
 INSERT INTO boards (user_account_id, title, content, hashtag, created_by, created_at, modified_by, modified_at)
-VALUES (1, '두 번째 게시글', '테스트용 내용입니다. 지금 듣고 있는 노래는 reality입니다.', '#reality', 'tester', NOW(), 'tester', NOW());
+VALUES (1, '두 번째 게시글', '테스트용 내용입니다. 지금 듣고 있는 노래는 Reality입니다.', '#reality', 'tester', NOW(), 'tester', NOW());
+
+INSERT INTO boards (user_account_id, title, content, hashtag, created_by, created_at, modified_by, modified_at)
+VALUES (1, '세 번째 게시글', '테스트용 내용입니다. 지금 듣고 있는 노래는 Bring the heat back입니다.', '#bringthehitback', 'tester', NOW(), 'tester', NOW());
+
+INSERT INTO boards (user_account_id, title, content, hashtag, created_by, created_at, modified_by, modified_at)
+VALUES (1, '네 번째 게시글', '테스트용 내용입니다. 지금 듣고 있는 노래는 One in a billion입니다.', '#oneinabillion', 'tester', NOW(), 'tester', NOW());
+
+INSERT INTO boards (user_account_id, title, content, hashtag, created_by, created_at, modified_by, modified_at)
+VALUES (1, '다섯 번째 게시글', '테스트용 내용입니다. 지금 듣고 있는 노래는 Whiplash입니다.', '#whiplash', 'tester', NOW(), 'tester', NOW());
+
+INSERT INTO boards (user_account_id, title, content, hashtag, created_by, created_at, modified_by, modified_at)
+VALUES (1, '여섯 번째 게시글', '테스트용 내용입니다. 지금 듣고 있는 노래는 Supernova입니다.', '#supernova', 'tester', NOW(), 'tester', NOW());
+
+INSERT INTO boards (user_account_id, title, content, hashtag, created_by, created_at, modified_by, modified_at)
+VALUES (1, '일곱 번째 게시글', '테스트용 내용입니다. 지금 듣고 있는 노래는 Loose입니다.', '#loose', 'tester', NOW(), 'tester', NOW());
+
+INSERT INTO boards (user_account_id, title, content, hashtag, created_by, created_at, modified_by, modified_at)
+VALUES (1, '여덟 번째 게시글', '테스트용 내용입니다. 지금 듣고 있는 노래는 잉걸입니다.', '#잉걸', 'tester', NOW(), 'tester', NOW());
+
+INSERT INTO boards (user_account_id, title, content, hashtag, created_by, created_at, modified_by, modified_at)
+VALUES (1, '아홉 번째 게시글', '테스트용 내용입니다. 지금 듣고 있는 노래는 Lucky입니다.', '#lucky', 'tester', NOW(), 'tester', NOW());
+
+INSERT INTO boards (user_account_id, title, content, hashtag, created_by, created_at, modified_by, modified_at)
+VALUES (1, '열 번째 게시글', '테스트용 내용입니다. 지금 듣고 있는 노래는 Candy입니다.', '#candy', 'tester', NOW(), 'tester', NOW());
+
+INSERT INTO boards (user_account_id, title, content, hashtag, created_by, created_at, modified_by, modified_at)
+VALUES (1, '열한 번째 게시글', '테스트용 내용입니다. 지금 듣고 있는 노래는 Blue입니다.', '#blue', 'tester', NOW(), 'tester', NOW());
 
 -- ArticleComments 테이블에 댓글 삽입 (boards_id가 외래키)
 INSERT INTO article_comments (boards_id, user_account_id, content, created_at, created_by, modified_at, modified_by)
@@ -19,3 +46,27 @@ VALUES (2, 1, '두 번째 게시글의 첫 번째 댓글입니다.', NOW(), 'tes
 
 INSERT INTO article_comments (boards_id, user_account_id, content, created_at, created_by, modified_at, modified_by)
 VALUES (2, 1, '두 번째 게시글의 두 번째 댓글입니다.', NOW(), 'tester', NOW(), 'tester');
+
+INSERT INTO article_comments (boards_id, user_account_id, content, created_at, created_by, modified_at, modified_by)
+VALUES (3, 1, '세 번째 게시글의 첫 번째 댓글입니다.', NOW(), 'tester', NOW(), 'tester');
+
+INSERT INTO article_comments (boards_id, user_account_id, content, created_at, created_by, modified_at, modified_by)
+VALUES (4, 1, '네 번째 게시글의 첫 번째 댓글입니다.', NOW(), 'tester', NOW(), 'tester');
+
+INSERT INTO article_comments (boards_id, user_account_id, content, created_at, created_by, modified_at, modified_by)
+VALUES (4, 1, '네 번째 게시글의 두 번째 댓글입니다.', NOW(), 'tester', NOW(), 'tester');
+
+INSERT INTO article_comments (boards_id, user_account_id, content, created_at, created_by, modified_at, modified_by)
+VALUES (7, 1, '일곱 번째 게시글의 첫 번째 댓글입니다.', NOW(), 'tester', NOW(), 'tester');
+
+INSERT INTO article_comments (boards_id, user_account_id, content, created_at, created_by, modified_at, modified_by)
+VALUES (8, 1, '여덟 번째 게시글의 첫 번째 댓글입니다.', NOW(), 'tester', NOW(), 'tester');
+
+INSERT INTO article_comments (boards_id, user_account_id, content, created_at, created_by, modified_at, modified_by)
+VALUES (10, 1, '열 번째 게시글의 첫 번째 댓글입니다.', NOW(), 'tester', NOW(), 'tester');
+
+INSERT INTO article_comments (boards_id, user_account_id, content, created_at, created_by, modified_at, modified_by)
+VALUES (11, 1, '열한 번째 게시글의 첫 번째 댓글입니다.', NOW(), 'tester', NOW(), 'tester');
+
+INSERT INTO article_comments (boards_id, user_account_id, content, created_at, created_by, modified_at, modified_by)
+VALUES (11, 1, '열한 번째 게시글의 두 번째 댓글입니다.', NOW(), 'tester', NOW(), 'tester');
